70ad4a135b30edb5f3f39829d55ad137f2e8dd07
microttus <martino@uia.no>
2024-10-21 18:14:11 +0200

    Test for all

--- mjolnir_arm_control/main.py
+++ mjolnir_arm_control/main.py
@@ -107,7 +107,7 @@ class ServoNode(Node):
             print("No valid solution")
 
         # Array the solution
-        servo_motor_pos = np.array([self.theta1, self.theta2, self.theta3, self.theta4, 90, 90])
+        servo_motor_pos = np.array([self.theta1, self.theta2, self.theta3, self.theta4, self.theta3, self.theta4])
 
         # Send servo positions
         try:
