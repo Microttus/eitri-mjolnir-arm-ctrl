7034e725aa6668082ff59e27f844a8f07331bbe5
microttus <martino@uia.no>
2024-10-22 14:25:48 +0200

    Added calculation for joint 4

--- mjolnir_arm_control/main.py
+++ mjolnir_arm_control/main.py
@@ -107,7 +107,7 @@ class ServoNode(Node):
             self.theta1 = int(solution[0][0]) + 90
             self.theta2 = int(solution[0][1]) + 90
             self.theta3 = int(solution[0][2]) + 180
-            self.theta4 = self.theta3                   # TODO: Add calc for this one
+            self.theta4 = (-90 - int(solution[0][1]) - int(solution[0][2])) + 180
             #print(f"1: {self.theta1}1 , 2: {theta2}, 3: {theta3}")
         else:
             print("No valid solution")
